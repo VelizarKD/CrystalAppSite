e93b142deb1a9aae1823c65afed044ba633c15dc
VelizarKD <vlzrft@gmail.com>
2023-03-01 14:32:33 +0200

    SetUp project

---
+++ src/main/java/com/example/crystal/CrystalApplication.java
@@ -0,0 +1,13 @@
+package com.example.crystal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrystalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrystalApplication.class, args);
+    }
+
+}
